688e69aa554e83523261edb23cf608250ac19afe
moonhy7 <moonhy7@naver.com>
2021-09-12 20:46:42 +0900

    21.9.12.Sun

--- 02. 자바스크립트 기초 문법/js/example.js
+++ 02. 자바스크립트 기초 문법/js/example.js
@@ -1,1 +1,1 @@
-document.write("환영합니다.")
+document.write("환영합니다.");
---
+++ 02. 자바스크립트 기초 문법/js/example2.js
@@ -0,0 +1,7 @@
+var i = prompt("나이는?", "0");
+
+if(i>=20) {
+    document.write("성인입니다.");
+} else {
+    document.write("미성년자입니다.");
+}
